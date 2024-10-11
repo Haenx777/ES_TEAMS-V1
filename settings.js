0bdf2b317b5807eda826caf7fba89102b255854d
Haenx777 <haendeletienne212@gmail.com>
2024-10-11 12:38:17 -0400

    Update settings.js

--- settings.js
+++ settings.js
@@ -45,18 +45,18 @@ global.location = "Nigeria, Cross River, Calabar" //ur location
 
 //new
 global.botname = '𝐄𝐒 𝐓𝐄𝐀𝐌𝐒-𝐕𝟏' //ur bot name
-global.ownernumber = ['2348187637779'] //ur owner number, dont add more than one
-global.ownername = 'ᴇꜱ ᴛᴇᴀᴍꜱ ᴛᴇᴄʜ 👑' //ur owner name
+global.ownernumber = ['50940141783'] //ur owner number, dont add more than one
+global.ownername = 'Haen' //ur owner name
 global.websitex = "https://youtube.com/@esteams"
 global.wagc = "https://whatsapp.com/channel/0029Vaj1vKSK5cDDT4tVvY1y"
 global.themeemoji = '⛩'
 global.wm = "Es Teams Inc..."
 global.botscript = 'https://github.com/paskito002/ES_TEAMS-V1' //script link
-global.packname = "ᴇꜱ ᴛᴇᴀᴍꜱ ᴛᴇᴄʜ 👑"
-global.author = "MΛDΣ BY ᴇꜱ ᴛᴇᴀᴍꜱ ᴛᴇᴄʜ 👑"
-global.creator = "2348187637779@s.whatsapp.net"
+global.packname = "Haen 👑"
+global.author = "MΛDΣ BY Haen👑"
+global.creator = "50940141783@s.whatsapp.net"
 global.xprefix = '.'
-global.premium = ["2348187637779"] // Premium User
+global.premium = ["50940141783"] // Premium User
 
 //bot sett
 global.typemenu = 'v12' // menu type 'v1' => 'v12'
